eaef0352d5a4cdba61f72a6853ac6396f83f6b68
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2018-08-14 23:54:13 +0700

    recruit

--- wp-content/themes/b4st-master/archive-recruit.php
+++ wp-content/themes/b4st-master/archive-recruit.php
@@ -17,11 +17,10 @@
                     'order' => 'DESC',
                     'posts_per_page' => 1000
                 ));
-                wp_get_post_terms($post_id, $taxonomy, $args);
 
                 ?>
                 <div class="row">
-                    <div class="col-sm recruit-box">
+                    <div class="container recruit-box">
                         <div class="recruit-list">
                             <div class="row recruit-header">
                                 <div class="col-sm-9 left">
@@ -47,6 +46,8 @@
                             <?php endwhile;
                             wp_reset_query(); ?>
                         </div>
+                        <p class="text-right m-4" ><img style="width: 25px; margin-right: 10px;" src="<?php echo get_template_directory_uri(); ?>/theme/images/download.png"><a style="color: rgba(245, 130, 32, 255);    padding: 3px;
+    border-bottom: 1px dotted; cursor: pointer;">Download CV Template</a> </p>
                     </div>
                 </div>
 
